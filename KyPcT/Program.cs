cfb34da135f883d5e936f79b31be8dea61144db0
Kadir Paşaoğlu <92451463+kadirn7@users.noreply.github.com>
2024-03-10 17:23:35 +0300

    Update Program.cs

--- KyPcT/Program.cs
+++ KyPcT/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-//deðiþiklik
+//deðiþiklik var
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
